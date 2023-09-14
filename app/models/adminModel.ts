dd35a506195d18c886b7e9305f11faee0af5b944
loveheenavasu <trivediheenalove@gmail.com>
2023-09-14 15:49:28 +0530

    feat:heroSection create api

--- app/models/adminModel.ts
+++ app/models/adminModel.ts
@@ -1,6 +1,16 @@
-import { Document } from "mongoose";
+import mongoose, { Document } from "mongoose";
 
 interface AdminDocument extends Document {
-    email:string;
-    password:string;
+  email: string;
+  password: string;
 }
+
+const adminSchema = new mongoose.Schema<AdminDocument>({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+export const adminModel = mongoose.model<AdminDocument>(
+  "adminPie",
+  adminSchema
+);
