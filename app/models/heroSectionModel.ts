1a657bd047f2d09f5ccee91b7dff1d8b0da3e4f0
loveheenavasu <loveheenavasu@tigerhall.com>
2023-09-17 08:50:29 +0530

    fix: changes related to base64

--- app/models/heroSectionModel.ts
+++ app/models/heroSectionModel.ts
@@ -3,19 +3,22 @@ import mongoose, { Document, Mongoose, ObjectId } from "mongoose";
 interface POSTImageSchema {
   imageUrl: string;
 }
-export interface POSTJSON extends Document{
-  heroSection: {imageUrl:POSTImageSchema};
+export interface POSTJSON extends Document {
+  heroSection: { imageUrl: POSTImageSchema };
   data: Array<POSTImageSchema>;
 }
 
 const heroSectionSchema = new mongoose.Schema<POSTJSON>(
   {
-    heroSection: {imageUrl:{ type: String ,required:true}},
-    data: [{imageUrl:{ type: String }}],
+    heroSection: { imageUrl: { type: String, required: true } },
+    data: [{ imageUrl: { type: String } }],
   },
   { timestamps: true }
 );
 
-const heroSectionModel=mongoose.model<POSTJSON>('heroSectionModel',heroSectionSchema);
+const heroSectionModel = mongoose.model<POSTJSON>(
+  "heroSectionModel",
+  heroSectionSchema
+);
 
-export {heroSectionModel}
+export { heroSectionModel };
